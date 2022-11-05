448d16a34dcb53e8c25edbd7ae50a1a5c5b923c0
Duke John Tiwari <duke.john90@gmail.com>
2022-11-05 17:06:35 +0530

    Adding files of project to git

---
+++ src/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+function Navbar(){
+    return (
+        <div>
+             <nav class="navbar navbar-dark bg-dark">
+  <div class="container-fluid">
+    <a class="navbar-brand">Choose Your Pokemon</a>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
+        </div>
+       
+    )
+}
+
+export default Navbar;
